6fdd99bcb7c678b8e423f7881fb03fd3ec662fc3
JesusOrt <xJesus_Nevermor3@hotmail.com>
2025-02-04 19:18:13 -0400

    cambios a los modulos

--- models/secciones.js
+++ models/secciones.js
@@ -1,35 +1,24 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
-const User = require('./user');
 
 const TextoDinamico = sequelize.define('TextoDinamico', {
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
     },
     seccion: {
-        type: DataTypes.STRING,
-        allowNull: false,
+    type: DataTypes.STRING,
+    allowNull: false,
     },
     contenido: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-    },
-usuarioId: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: User,
-            key: 'id',
-        },
-        allowNull: false,
+    type: DataTypes.TEXT,
+    allowNull: false,
     },
 }, {
     tableName: 'textosDinamicos',
     timestamps: false,
 });
 
-User.hasMany(TextoDinamico, { foreignKey: 'id' });
-TextoDinamico.belongsTo(User, { foreignKey: 'id' });
 
 module.exports = TextoDinamico;
